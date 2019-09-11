dc46b1141a37521162eeba41f1850d3cacdbaa50
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-11 23:08:26 +0300

    Add 'duplicate_driver_license' processing.

--- src/app/Http/Controllers/ApiController.php
+++ src/app/Http/Controllers/ApiController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use App\Lead;
 use Exception;
 use Illuminate\Http\Request;
+use Likemusic\YandexFleetTaxi\FrontendData\Contracts\DriverLicenseInterface;
 use Likemusic\YandexFleetTaxiClient\Contracts\ClientInterface as YandexClientInterface;
 use App\Http\Requests\Lead as LeadFormRequest;
 use Illuminate\Validation\ValidationException;
 use Likemusic\YandexFleetTaxi\FrontendData\ToYandexClientPostDataConverters\Converter\ToCreateDriver as ToCreateDriverPostDataConverter;
 use Likemusic\YandexFleetTaxiClient\Exception as YandexClientException;
+use Likemusic\YandexFleetTaxiClient\HttpJsonResponseException;
+use Likemusic\YandexFleetTaxiClient\HttpResponseException;
 use Psr\Http\Message\RequestInterface;
 
 class ApiController extends Controller
@@ -45,7 +48,8 @@ class ApiController extends Controller
         string $yandexPassword,
         YandexClientInterface $yandexClient,
         ToCreateDriverPostDataConverter $toCreateDriverPostDataConverter
-    ) {
+    )
+    {
         $this->parkId = $parkId;
         $this->yandexLogin = $yandexLogin;
         $this->yandexPassword = $yandexPassword;
@@ -56,7 +60,7 @@ class ApiController extends Controller
     public function addLead(LeadFormRequest $request)
     {
         $status = 'error';
-
+        $errors = null;
         $ret = [];
 
         try {
@@ -73,17 +77,89 @@ class ApiController extends Controller
 //            $this->bindCarToDriver($driverId, $carId);
             $status = 'success';
             $ret = ['status' => 'success'];
-        }
-        catch (Exception $exception) {
+        } catch (HttpJsonResponseException $exception) {
+            $this->logHttpJsonResponseException($exception);
+            $errors = $this->getErrorsByHttpJsonResponseException($exception);
+        } catch (HttpResponseException $exception) {
+            $this->logHttpResponseException($exception);
+            $errors = $this->getErrorsByHttpResponseException($exception);
+        } catch (Exception $exception) {
             $this->logException($exception, $request);
+            $errors = $this->getErrorsByException($exception);
+        }
 
-            //list($errorFieldName, $errorMessage) = $this->getErrorFieldNameAndMessageByException($exception);
-            $ret['errors']['common'][] =  'Во время обработки формы произошла ошибка. Попробуйте повторить отправку формы еще раз немного позже.';
+        if ($errors) {
+            $ret['errors'] = $errors;
         }
 
         $ret['status'] = $status;
 
-        return $this->createJsonResponse($ret);
+        return $this->createJsonResponse($ret, $errors);
+    }
+
+    protected function getErrorsByException(Exception $exception)
+    {
+        return [
+            'common' => [$exception->getMessage()],
+        ];//todo
+    }
+
+    private function getErrorsByHttpResponseException(HttpResponseException $exception)
+    {
+        return [];//todo
+    }
+
+    private function getErrorsByHttpJsonResponseException(HttpJsonResponseException $exception)
+    {
+        if ($this->isRequestValidationErrorException($exception)) {
+            return $this->getRequestValidationErrorResponseErrors($exception);
+        }
+
+        $jsonCode = $exception->getJsonCode();
+
+        switch ($jsonCode) {
+            case 'duplicate_driver_license':
+                $message = 'Водитель с указанным ВУ уже зарегистрирован.';
+                $errors = [
+                    DriverLicenseInterface::SERIES => [$message],
+                    DriverLicenseInterface::NUMBER => [$message],
+                ];
+                break;
+            default:
+                $errors = [
+                    'common' => [
+                        'Во время обработки запроса произошла ошибка. Попробуйте повторить отправку формы немного позже.'
+                    ]
+                ];
+        }
+
+        return $errors;
+    }
+
+    private function getRequestValidationErrorResponseErrors(HttpJsonResponseException $exception)
+    {
+        return [
+            'common' => [
+                'Во время обработки запроса произошла ошибка. Попробуйте повторить отправку формы немного позже. Если ошибка повторяется, сообщите об это владельцу сайта.'
+            ]
+        ];
+    }
+
+    private function isRequestValidationErrorException(HttpJsonResponseException $exception)
+    {
+        return $exception->getCode() === 400
+            && $exception->getJsonCode() === 'REQUEST_VALIDATION_ERROR'
+            && $exception->getJsonMessage() === 'Some parameters are invalid';
+    }
+
+    private function logHttpResponseException(HttpResponseException $exception)
+    {
+        //todo
+    }
+
+    private function logHttpJsonResponseException(HttpJsonResponseException $exception)
+    {
+        //todo
     }
 
     private function logException(Exception $exception, LeadFormRequest $request)
@@ -133,8 +209,10 @@ class ApiController extends Controller
         return ['common', $exception->getMessage()];//todo
     }
 
-    private function createJsonResponse($data)
+    private function createJsonResponse($data, $error = false)
     {
-        return response()->json($data);
+        $status = !$error ? 200 : 422;
+
+        return response()->json($data, $status);
     }
 }
--- src/app/Http/Requests/Lead.php
+++ src/app/Http/Requests/Lead.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Likemusic\YandexFleetTaxi\FrontendData\Contracts\DriverInterface;
+use Likemusic\YandexFleetTaxi\FrontendData\Contracts\DriverLicenseInterface;
+use Likemusic\YandexFleetTaxi\FrontendData\Contracts\CarInterface;
 
 class Lead extends FormRequest
 {
@@ -19,15 +21,23 @@ class Lead extends FormRequest
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * @see https://docs.google.com/spreadsheets/d/1QPisHDS7YYXGf5kcqXhvav2fDIBGZoAbOhethOog2ZI/edit#gid=1479188633
      * @return array
      */
     public function rules()
     {
         return [
+            // Driver
             DriverInterface::FIRST_NAME => 'required',
             DriverInterface::LAST_NAME => 'required',
-            DriverInterface::MIDDLE_NAME => 'required',
+            DriverInterface::WORK_PHONE => 'required',
+
+            // Driver License
+            DriverLicenseInterface::ISSUE_COUNTRY => 'required',
+            DriverLicenseInterface::EXPIRATION_DATE => 'required',
+            DriverLicenseInterface::ISSUE_DATE => 'required',
+            DriverLicenseInterface::SERIES => 'required',
+            DriverLicenseInterface::NUMBER => 'required',
         ];
     }
 
@@ -39,9 +49,16 @@ class Lead extends FormRequest
     public function attributes()
     {
         return [
-            DriverInterface::FIRST_NAME => 'Имя',
-            DriverInterface::LAST_NAME => 'Фамилия',
-            DriverInterface::MIDDLE_NAME => 'Отчество',
+            DriverInterface::FIRST_NAME => '"Имя"',
+            DriverInterface::LAST_NAME => '"Фамилия"',
+            DriverInterface::MIDDLE_NAME => '"Отчество"',
+            DriverInterface::WORK_PHONE => '"Номер рабочего телефона"',
+
+            DriverLicenseInterface::ISSUE_COUNTRY => '"Страна выдачи ВУ"',
+            DriverLicenseInterface::EXPIRATION_DATE => '"Дата окончания действия ВУ"',
+            DriverLicenseInterface::ISSUE_DATE => '"Дата выдачи ВУ"',
+            DriverLicenseInterface::SERIES => '"Серия ВУ"',
+            DriverLicenseInterface::NUMBER => '"Номер ВУ"',
         ];
     }
 }
